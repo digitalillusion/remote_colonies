49ab885f74a7b00a86998021fc5d6fc41bf58f60
digitalillusion <adriano.dalpane@gmail.com>
2020-06-09 20:46:22 +0200

    planet resources allocation

--- src/renderer/godot2d/mod.rs
+++ src/renderer/godot2d/mod.rs
@@ -18,7 +18,8 @@ pub enum ManageErrs {
 pub struct Main {
     #[property]
     planet: PackedScene,
-    starmap: Option<Starmap<Area2D>>,
+    starmap: Option<Starmap<Node2D>>,
+    players: List<Player<Node2D>>,
 }
 
 #[methods]
@@ -35,17 +36,16 @@ impl Main {
     unsafe fn _ready(&mut self, mut owner: Node) {
         let starmap = Starmap::new(10)
             .with_generator(|id| {
-                let planet_area2d: Area2D = instance_scene(&self.planet).unwrap();
-                owner.add_child(Some(planet_area2d.to_node()), false);
+                let planet_node: Node2D = instance_scene(&self.planet).unwrap();
+                owner.add_child(Some(planet_node.to_node()), false);
 
-                let planet_instance = Instance::<Planet>::try_from_unsafe_base(planet_area2d).unwrap();
-                planet_instance.map(|planet, planet_owner| {
-                    let label = &format!("{}", id).to_string();
+                let planet_instance = Instance::<Planet>::try_from_unsafe_base(planet_node).unwrap();
+                planet_instance.map_mut(|planet, planet_owner| {
                     planet.set_random_features(planet_owner);
-                    planet.set_label(label, planet_owner);
+                    planet.set_id(id);
                 }).unwrap();
 
-                planet_area2d
+                planet_node
             })
             .with_validator(|planet1, planet2| {
                 let distance = Main::distance_between(planet1, planet2);
@@ -53,20 +53,28 @@ impl Main {
             })
             .with_cleaner(|planet| planet.free())
             .build();
-        starmap.get_planets_by_max_distance(3, |planet1, planet2| {
+        starmap.get_planets_by_max_distance(2, |planet1, planet2| {
             Main::distance_between(planet1, planet2)
-        }).iter().for_each(|planet| {
-            let planet_label: Label = planet
-                .find_node(GodotString::from_str("Label"), false, true)
-                .expect("Unable to find planet/Label")
-                .cast()
-                .expect("Unable to cast to Label");
-            godot_print!("{}", planet_label.get_text());
+        }).iter().for_each(|planet_node| {
+            let planet_instance = Instance::<Planet>::try_from_unsafe_base(**planet_node).unwrap();
+                planet_instance.map_mut(|planet, _planet_owner| {
+                    planet.set_resources(100.0, 1.02);
+                }).unwrap();
+
+            let ship_node: Node2D = instance_scene(&self.planet).unwrap();
+            owner.add_child(Some(planet_node.to_node()), false);
+
+            let planet_instance = Instance::<Planet>::try_from_unsafe_base(planet_node).unwrap();
+            planet_instance.map_mut(|planet, planet_owner| {
+                planet.set_random_features(planet_owner);
+                planet.set_id(id);
+            }).unwrap();
         });
         self.starmap = Some(starmap);
     }
 
-    unsafe fn distance_between(planet1: &Area2D, planet2: &Area2D) -> f32 {
+
+    unsafe fn distance_between(planet1: &Node2D, planet2: &Node2D) -> f32 {
         let p1pos = Point2::new(planet1.get_position().x, planet1.get_position().y);
         let p2pos = Point2::new(planet2.get_position().x, planet2.get_position().y);
         p1pos.distance_to(p2pos)
--- src/renderer/godot2d/planet.rs
+++ src/renderer/godot2d/planet.rs
@@ -3,51 +3,78 @@ use gdnative::*;
 use rand::*;
 
 #[derive(NativeClass)]
-#[inherit(Area2D)]
-pub struct Planet;
+#[inherit(Node2D)]
+pub struct Planet {
+    id: usize,
+    resources: f32,
+    resources_increase_ratio: f32,
+}
 
 #[methods]
 impl Planet {
     
-    fn _init(_owner: Area2D) -> Planet {
-        Planet
+    fn _init(_owner: Node2D) -> Planet {
+        let mut rng = rand::thread_rng();
+
+        Planet {
+            id: 0,
+            resources: rng.gen_range(10.0, 250.0),
+            resources_increase_ratio: 1.0 + rng.gen_range(0.0002, 0.005)
+        }
     }
     
     #[export]
-    unsafe fn _ready(&self, _owner: Area2D) {
+    unsafe fn _ready(&self, _owner: Node2D) {
     }
 
-    pub unsafe fn set_random_features(&self, mut owner: Area2D) {
-        let planet_sprite: Sprite = owner
-            .find_node(GodotString::from_str("Sprite"), false, true)
-            .expect("Unable to find planet/Sprite")
+    pub unsafe fn set_random_features(&self, mut owner: Node2D) {
+        let mut planet_shape: Area2D = owner
+            .find_node(GodotString::from_str("Shape"), false, true)
+            .expect("Unable to find planet/Shape")
             .cast()
-            .expect("Unable to cast to Sprite");
+            .expect("Unable to cast to Shape");
 
         let viewport_rect: Rect2 = owner.get_viewport_rect();
         let viewport_width = viewport_rect.width();
         let viewport_height = viewport_rect.height();
 
         let mut rng = rand::thread_rng();
+        let planet_sprite: Sprite = planet_shape
+            .find_node(GodotString::from_str("Sprite"), false, true)
+            .expect("Unable to find planet/Shape")
+            .cast()
+            .expect("Unable to cast to Sprite");
         let size = planet_sprite.get_texture()
             .expect("Unable to get Texture")
             .get_width() as f32;
         let scale = rng.gen_range(0.2, 1.0);
+        planet_shape.set_scale(Vector2::new(scale, scale));
+
         let border = scale * size * 0.5;
         let x_offset = (rng.gen_range(0.0, 1.0) * viewport_width).clamp(border, viewport_width - border);
         let y_offset = (rng.gen_range(0.0, 1.0) * viewport_height).clamp(border, viewport_height - border);
-        
-        owner.set_scale(Vector2::new(scale, scale));
         owner.set_position(Vector2::new(x_offset, y_offset));
     }
 
-    pub unsafe fn set_label(&self, text: &str, owner: Area2D) {
+    #[export]
+    pub unsafe fn on_resource_timer_timeout(&mut self, owner: Node2D) {
+        self.resources *= self.resources_increase_ratio;
+        
+        let label = &format!("{} - {}", self.id.to_string(), (self.resources as usize).to_string());
         let mut planet_label: Label = owner
             .find_node(GodotString::from_str("Label"), false, true)
             .expect("Unable to find planet/Label")
             .cast()
             .expect("Unable to cast to Label");
+        planet_label.set_text(GodotString::from_str(label));
+    }
 
-        planet_label.set_text(GodotString::from_str(text));
+    pub fn set_id(&mut self, id: usize) {
+        self.id = id;
+    }
+
+    pub fn set_resources(&mut self, initial: f32, inc: f32) {
+        self.resources = initial;
+        self.resources_increase_ratio = inc;
     }
 }
